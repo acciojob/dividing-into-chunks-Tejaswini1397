72586071127964c8ecf54f3eb20b2217fe3bb98e
Tejaswini Khairnar <117677072+Tejaswini1397@users.noreply.github.com>
2023-11-18 09:46:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,17 +2,28 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
-	let result=[];
-	let subArray=[];
-	for(let i=0;i<srr.lenght;i++){
-		if(subArray.reduce((sum,num)=> sum+num,0)+arr[i]<n){
-			result.push(subArray);
-			subArray=arr[i];
-		}else{
-			subArray.push(arr[i]);
-		}
-	}
-	result.push(subArray);
+	
+	let result = [];
+  let currentSubarray = [];
+  let currentSum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    // Check if adding the current element exceeds the maximum sum
+    if (currentSum + arr[i] > n) {
+      // If yes, start a new subarray
+      result.push(currentSubarray);
+      currentSubarray = [arr[i]];
+      currentSum = arr[i];
+    } else {
+      // If no, add the current element to the current subarray
+      currentSubarray.push(arr[i]);
+      currentSum += arr[i];
+    }
+  }
+
+  // Add the last subarray to the result
+  result.push(currentSubarray);
+
 	return result;
 	
 };
