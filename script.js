a11d4daf983e4582beb5dcc78f4cbd10f0713d7a
Tejaswini Khairnar <117677072+Tejaswini1397@users.noreply.github.com>
2023-11-18 09:43:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+	let result=[];
+	let subArray=[];
+	for(let i=0;i<srr.lenght;i++){
+		if(subArray.reduce((sum,num)=> sum+num,0)+arr[i]>n){
+			result.push(subArray);
+			subArray=arr[i];
+		}else{
+			subArray.push(arr[i]);
+		}
+	}
+	result.push(subArray);
+	return result;
+	
 };
 
 const n = prompt("Enter n: ");
